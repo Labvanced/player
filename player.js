19298c8e2f791be1c3bd43b771899472b9753c1d
Holger Finger <hfinger@users.noreply.github.com>
2016-01-19 17:25:58 +0100

    recording of experiments...

--- player.js
+++ player.js
@@ -5,16 +5,35 @@ var Player = function() {
 
     this.expId = location.search.split('id=')[1];
     this.experiment = null;
+    this.sessionNr = 0;
 
-    console.log("requesting experiment with id "+this.expId+" from server.")
+    console.log("requesting experiment with id "+this.expId+" from server.");
 
     var parameters = { expId: this.expId };
     $.get('/getExperiment', parameters, function(data){
         console.log("experiment spec loaded from server.");
+        self.sessionNr = data.sessionNr;
         self.experiment = new Experiment().fromJS(data.expData);
         self.experiment.setPointers();
         console.log("experiment deserialized.");
+
+        self.addRecording(0,0,{
+            testData: 12345
+        })
+
     });
+
+};
+
+Player.prototype.addRecording = function(blockNr, trialNr, recData) {
+
+    var recordData = {
+        blockNr: blockNr,
+        trialNr: trialNr,
+        recData: recData
+    };
+
+    $.post('/record', recordData);
 
 };
 
